0d9e9506131726d3016d033e12c985a4e9608ef0
boben195 <boben195@ukr.net>
2025-08-11 21:22:27 +0300

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
+import { Routes, Route } from "react-router";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import Addbook from "./pages/Addbook";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+        <Route path={"/add-book"} element={<Addbook />} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/signup"} element={<SignupPage />} />
+      </Routes>
     </>
   );
 }
