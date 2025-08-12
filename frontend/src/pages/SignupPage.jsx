6701e8b5a029ff9134fc666304f14eea1510adcb
boben195 <boben195@ukr.net>
2025-08-12 19:51:56 +0300

    add signup

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form action="">
+        <div>
+          <label htmlFor="">Username</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label htmlFor="">Password</label>
+          <input type="password" />
+        </div>
+        <div>
+          <label htmlFor="">Confirm Password</label>
+          <input type="password" />
+        </div>
+        <button>Sign Up</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignupPage;
