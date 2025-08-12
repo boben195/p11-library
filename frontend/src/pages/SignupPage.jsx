ae51e7b1607ca68c6b2362ffec82e0cf309742c3
boben195 <boben195@ukr.net>
2025-08-12 20:00:14 +0300

    style for sign up

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -2,26 +2,42 @@ import React from "react";
 
 const SignupPage = () => {
   return (
-    <div>
-      <h2>Sign Up</h2>
-      <form action="">
-        <div>
-          <label htmlFor="">Username</label>
-          <input type="text" />
+    <div className="min-h-screen text-[#252422] bg-[#f5f5f5] px-4 md:px-12">
+      <h2 className="text-center font-semibold pt-8 md:text-2xl w-full max-w-xl mx-auto">
+        Sign Up
+      </h2>
+      <form className="flex flex-col justify-center items-center w-full max-w-xl mx-auto space-y-4 mt-10">
+        <div className="flex flex-col w-full">
+          <label className="md:text-lg">Username</label>
+          <input
+            type="text"
+            className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-white border border-gray-500"
+          />
         </div>
-        <div>
-          <label htmlFor="">Email</label>
-          <input type="email" />
+        <div className="flex flex-col w-full">
+          <label className="md:text-lg">Email</label>
+          <input
+            type="email"
+            className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-white border border-gray-500"
+          />
         </div>
-        <div>
-          <label htmlFor="">Password</label>
-          <input type="password" />
+        <div className="flex flex-col w-full">
+          <label className="md:text-lg">Password</label>
+          <input
+            type="password"
+            className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-white border border-gray-500"
+          />
         </div>
-        <div>
-          <label htmlFor="">Confirm Password</label>
-          <input type="password" />
+        <div className="flex flex-col w-full">
+          <label className="md:text-lg">Confirm Password</label>
+          <input
+            type="password"
+            className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-white border border-gray-500"
+          />
         </div>
-        <button>Sign Up</button>
+        <button className="w-full bg-[#403D39] text-[#FFFCF2] py-2 font-medium rounded-lg">
+          Sign Up
+        </button>
       </form>
     </div>
   );
