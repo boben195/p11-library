7de3d00fbfb2374693e74c20628b39563c9bb075
boben195 <boben195@ukr.net>
2025-08-13 19:31:05 +0300

    add base for back

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+
+app.listen(PORT, () => {
+    console.log("Server started at PORT: ", PORT);
+    
+})
