7bdcd32c3171fe74671f99bc91089168ca439000
boben195 <boben195@ukr.net>
2025-08-13 19:38:21 +0300

    test

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-import express from "express";
+// import express from "express";
 
 
-const app = express();
-const PORT = 5000;
+// const app = express();
+// const PORT = 5000;
 
-app.use(express.json());
-
-
-app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+// app.use(express.json());
 
 
-app.listen(PORT, () => {
-    console.log("Server started at PORT: ", PORT);
+// app.get("/", (req, res) => {
+//     res.send("Hello world")
+// })
+
+
+// app.listen(PORT, () => {
+//     console.log("Server started at PORT: ", PORT);
     
-})
+// })
