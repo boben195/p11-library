5f417099758df6ebcabc1c56032ce0c6b7d7c78f
boben195 <boben195@ukr.net>
2025-08-14 20:43:22 +0300

    test

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,15 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
 
-// export async function connectionDB() {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGO_URI)
-//         console.log("MongoDB connected: ", conn.connection.host);
+export async function connectionDB() {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB connected: ", conn.connection.host);
         
-//     } catch (error) {
-//         console.log("Error with connection", error);
-//         process.exit(1)
-//     }
-// }
+    } catch (error) {
+        console.log("Error with connection", error);
+        process.exit(1)
+    }
+}
 
 
