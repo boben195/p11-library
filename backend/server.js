56caea5fb78aada13e835436077d962618d71d2d
boben195 <boben195@ukr.net>
2025-08-14 20:37:27 +0300

    test

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+dotenv.config();
 
-// Try backend/.env first, otherwise fallback to project root
-dotenv.config({
-  path: path.join(__dirname, ".env"), // backend/.env
-});
-if (!process.env.PORT) {
-  dotenv.config({
-    path: path.join(__dirname, "../.env"), // root/.env
-  });
-}
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-console.log("PORT is", process.env.PORT);
+const PORT = process.env.PORT || 5000
+
+console.log("PORT is ", process.env.PORT);
+
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
+  res.send("Hello world")
+})
 app.listen(PORT, () => {
-  console.log("SERVER STARTED AT PORT", PORT);
-});
+    console.log("SERVER STARTED AT PORT ", PORT);
+    
+})
