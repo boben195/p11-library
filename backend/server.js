bdfb3313acfbebb0ab1ffdb1541d5f59fe636227
boben195 <boben195@ukr.net>
2025-08-16 20:44:02 +0300

    work with istakes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectionDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -13,10 +14,11 @@ console.log("PORT is ", process.env.PORT);
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello world")
-})
-app.listen(PORT, () => {
+// app.get("/", (req, res) => {
+//   res.send("Hello world")
+// })
+app.listen(PORT, async () => {
+  await connectionDB()
     console.log("SERVER STARTED AT PORT ", PORT);
     
 })
