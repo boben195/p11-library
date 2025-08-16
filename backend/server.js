e4e11b57db3b8c03ee9dfe7fc4117e7947594f24
boben195 <boben195@ukr.net>
2025-08-16 21:09:05 +0300

    add signup func

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectionDB } from "./config/db.js";
+import User from "./models/user.model.js";
+import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 dotenv.config();
 
@@ -20,9 +23,38 @@ app.post("/api/signup", async (req, res) => {
     if (!username || !email || !password) {
       throw new Error("All fields required")
     }
-    // const emailExists = 
+
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({message: "user already exist"})
+    }
+
+    const usernameExist = await User.findOne({ username });
+    if (usernameExist) {
+      return res.status(400).json({message: "username already exist, try another name"})
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const userDoc = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    })
+
+    if (userDoc) {
+      const token = jwt.sign({ id: userDoc._id }, process.env.JWT_SECRET, { expiresIn: "7d" })
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
+    }
+
+    return res.status(200).json({user: userDoc, message: "User created successfully"})
   } catch (error) {
-    console.log(error);
+    res.status(400).json({message: error.message})
   }
 })
 
