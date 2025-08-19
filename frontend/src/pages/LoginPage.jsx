da94a6dc5c40e4b1ff871ef3c86e65b4671c2146
boben195 <boben195@ukr.net>
2025-08-19 20:33:32 +0300

    finish with login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,17 +1,40 @@
-import React from "react";
-import { Link } from "react-router";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { useAuthStore } from "../store/authStore";
+import toast from "react-hot-toast";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isLoading, error } = useAuthStore();
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const { message } = await login(email, password);
+      toast.success(message);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="min-h-screen text-[#252422] bg-[#f5f5f5] px-4 md:px-12">
       <h2 className="text-center font-semibold pt-8 md:text-2xl w-full max-w-xl mx-auto">
         Log In
       </h2>
-      <form className="flex flex-col justify-center items-center w-full max-w-xl mx-auto space-y-4 mt-10">
+      <form
+        onSubmit={handleLogin}
+        className="flex flex-col justify-center items-center w-full max-w-xl mx-auto space-y-4 mt-10"
+      >
         <div className="flex flex-col w-full">
           <label className="md:text-lg">Email</label>
           <input
             type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-white border border-gray-500"
           />
         </div>
@@ -19,12 +42,20 @@ const LoginPage = () => {
           <label className="md:text-lg">Password</label>
           <input
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-white border border-gray-500"
           />
         </div>
 
-        <button className="w-full bg-[#403D39] text-[#FFFCF2] py-2 font-medium rounded-lg">
-          Log In
+        {error && <p className="text-red-500">{error}</p>}
+
+        <button
+          type="submit"
+          disabled={isLoading}
+          className="w-full bg-[#403D39] text-[#FFFCF2] py-2 font-medium rounded-lg"
+        >
+          {isLoading ? "Please wait..." : "Log in"}
         </button>
 
         <p>
